e972acfe8d4ce1dcc17a9a1828bf1d21e4520a13
Ali Amaan <aliamaan071@gmail.com>
2021-10-07 00:40:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const textInput = document.querySelector("#text-area-input");
-const textOutput = document.querySelector("#text-area-output");
-const btnTranslate = document.querySelector("#btn-translate");
+const textInput = document.querySelector("#text-area-input")
+const textOutput = document.querySelector("#text-area-output")
+const btnTranslate = document.querySelector("#btn-translate")
 
-let url = "https://api.funtranslations.com/translate/groot.json";
+let url = "https://api.funtranslations.com/translate/russian-accent.json"
 
 const getUrl = (text) => {
-  return url + "?text=" + text;
-};
+  return url + "?text=" + text
+}
 
 const showError = (error) => {
   alert(
     "Uh Oh! Some error occured\n\n" + error + "\n\nPlease try after sometime."
-  );
-  console.log({ error });
-};
+  )
+  console.log({ error })
+}
 
 const translate = () => {
-  const text = textInput.value;
+  const text = textInput.value
 
   fetch(getUrl(text))
     .then((response) => response.json())
     .then((json) => showOutput(json))
-    .catch(showError);
-};
+    .catch(showError)
+}
 
 const showOutput = (text) => {
-  console.log(text);
-  textOutput.innerHTML = text.contents.translated;
-};
+  console.log(text)
+  textOutput.innerHTML = text.contents.translated
+}
 
-btnTranslate.addEventListener("click", translate);
+btnTranslate.addEventListener("click", translate)
